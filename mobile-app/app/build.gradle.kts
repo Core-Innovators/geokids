08d21ecc3900090830deaf523a206026cfb705e6
T M Deneth Keshan Chathuranga <denethkc@gmail.com>
2025-10-10 00:06:16 +0530

    login and signup setup

--- mobile-app/app/build.gradle.kts
+++ mobile-app/app/build.gradle.kts
@@ -33,12 +33,23 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.13.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
+
+    // Firebase BOM (keeps versions in sync)
     implementation(platform("com.google.firebase:firebase-bom:34.3.0"))
+
+    // Firestore
+    implementation("com.google.firebase:firebase-firestore")
+
+    // Firebase Authentication
+    implementation("com.google.firebase:firebase-auth")
+
+    // (Optional) Google Sign-In support
+    implementation("com.google.android.gms:play-services-auth:21.2.0")
 }
