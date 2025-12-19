0be6d52d7e93ddeef3b53af7642a24eb5304c2d6
T M Deneth Keshan Chathuranga <denethkc@gmail.com>
2025-12-19 10:16:32 +0530

    Create driver active dashboard

---
+++ mobile-app/app/src/main/java/com/coreinnovators/geokids/QR_scan.java
@@ -0,0 +1,14 @@
+package com.coreinnovators.geokids;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QR_scan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_scan);
+    }
+}
---
+++ mobile-app/app/src/main/java/com/coreinnovators/geokids/driver_map.java
@@ -0,0 +1,14 @@
+package com.coreinnovators.geokids;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class driver_map extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_map);
+    }
+}
